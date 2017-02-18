5bcd412cd5584a211643a46dabba90a8dc1c6204
Felix Fong <moongod101@hotmail.com>
2017-02-18 21:20:43 +0800

    add basic page showing function

--- bin/index.js
+++ bin/index.js
@@ -4,16 +4,27 @@ const fs = require("fs")
     ,express = require("express")
     ,app = express()
     ,ejs = require("ejs")
+    ,bodyParser = require('body-parser');
 ;
 
-let slidePages = [];
-const slideConfig = packageJSON.slideConfig;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+// in latest body-parser use like below.
+app.use(bodyParser.urlencoded({ extended: true }));
 
+let slidePages = [];
+let currentPage = 0;
+const slideConfig = packageJSON.slideConfig;
 app.set("view engine", "ejs");
 app.set("views", path.join(`${__dirname}/../${slideConfig.pageRoot}`));
+app.use('/static', express.static(path.join(__dirname, '/../static')));
+
+const slides = slideConfig.slides;
+
+
 // Put all the slides path to the slidePages array
 packageJSON.slideConfig.slides.map(name => {
-    const pagePath = path.join(`${__dirname}/../${slideConfig.pageRoot}/${name}.html`);
+    const pagePath = path.join(`${__dirname}/../${slideConfig.pageRoot}/${name}.ejs`);
     slidePages.push(pagePath)
 });
 // Start up the web page
@@ -23,18 +34,42 @@ app.get("/", (req,res) => {
     res.render(`${__dirname}/../init/index`, {
         title: slideConfig.name
         ,appName: packageJSON.name
+        ,port: slideConfig.port
     });
 
 });
 
-app.post("/api", (req,res) => {
+app.get("/api", (req,res) => {
+
+    const query = req.query;
+    const load = query.load;
+
+    if(load == "forward"){
+
+        if(currentPage < slidePages.length){
+            const pagePath = path.join(slidePages[currentPage]);
+            currentPage++;
+            res.render(pagePath)
+        }
+
+    }else if(load == 'backward'){
+
+        if(currentPage <= slidePages.length){
+            currentPage--;
+            const pagePath = path.join(slidePages[currentPage]);
+            res.render(pagePath)
+        }
 
 
+    }
 
 });
 
 app.listen(slideConfig.port, () => {
 
-    console.log(`${packageJSON.name} is serving web server at port ${slideConfig.port}`)
+    console.log(`${packageJSON.name} is serving web server at port ${slideConfig.port}`);
+    // Start the browser
+    // require("open")(`http://localhost:${slideConfig.port}`);
+    // Sorry, maybe I'm just really too lazy to write the OS check script :P
 
 });
--- static/js/getPage.js
+++ static/js/getPage.js
@@ -1,3 +1,40 @@
-/**
- * Created by moong on 2/13/2017.
- */
+// When the user click the web page
+const ajax = new XMLHttpRequest();
+
+// Get forward page
+window.addEventListener("click", e => {
+
+    ajax.open("GET",`http://localhost:${config.port}/api?load=forward`);
+    ajax.setRequestHeader("Content-Type", "application/json");
+    ajax.send();
+    ajax.onreadystatechange = () => {
+
+        if(ajax.status === 200 && ajax.readyState === 4) {
+
+            document.querySelector("#main").innerHTML = ajax.response;
+
+        }
+
+    }
+
+
+});
+
+// Get backward page
+window.addEventListener("contextmenu", e => {
+    e.preventDefault();
+    ajax.open("GET",`http://localhost:${config.port}/api?load=backward`);
+    ajax.setRequestHeader("Content-Type", "application/json");
+    ajax.send();
+    ajax.onreadystatechange = () => {
+
+        if(ajax.status === 200 && ajax.readyState === 4) {
+
+            document.querySelector("#main").innerHTML = ajax.response;
+
+        }
+
+    }
+
+
+});
